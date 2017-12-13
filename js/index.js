d9cb66ab686b9585a9894572321d9e8b17a3ed65
Helen <maher.olena96@gmail.com>
2017-12-13 17:19:22 +0200

    added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-loadResponces();
+  loadResponces();
 
 
   function loadResponces() {
@@ -8,7 +8,6 @@ loadResponces();
       type: 'GET',
       url: './data/response.json',
       success: function(data) {
-        // clearResponces();
         printResponses(data);
       },
       error: function() {
@@ -17,53 +16,87 @@ loadResponces();
     });
   }
 
-  // function clearResponces() {
-  //   $('.item') each(function() {
-  //     $(this.remove());
-  //   });
-  // }
   function printResponses(dataArr) {
     dataArr.forEach(function(item) {
-      // if (item.selected === 'true') {
-      console.log(item);
-        $('<div class="item">')
-          .addClass('selected')
+      let item_elem = $('<div class="item">');
+      if (item.selected) {
+        item_elem.addClass('selected');
+      } else {
+        item_elem.addClass('unselected');
+      }
+      item_elem
+        .append(
+          $('<div class="content-wrapper">')
           .append(
-            $('<div class="content-wrapper">')
+            $('<div class="left-content">')
             .append(
-              $('<div class="left-content">')
+              $('<p class="circle">')
+            )
+            .append(
+              $('<p class="item-name">').text(item.menuOption.name)
+            )
+          )
+          .append(
+            $('<div class="right-content">')
+            .append(
+              $('<p class="price">').text('\u00A3' + item.menuOption.price)
               .append(
-                $('<p class="circle">')
-              )
-              .append(
-                $('<p class="item-name">').text(item.menuOption.name)
+                $('<span class="currency">')
               )
             )
             .append(
-              $('<div class="right-content">')
+              $('<button type="button" class="edit-icon">')
               .append(
-                $('<p class="price">').text(item.menuOption.price)
-                .append(
-                  $('<span class="currency">')
-                )
-              )
-              .append(
-                $('<button type="button" class="edit-icon">')
-                .append(
-                  $('<img src="img/icons/edit-icon.svg" alt="edit-icon">')
-                )
-              )
-              .append(
-                $('<button type="button" class="delete-icon">')
-                .append(
-                  $('<img src="img/icons/delete-icon.svg" alt="delete-icon">')
-                )
+                $('<img src="img/icons/edit-icon.svg" alt="edit-icon">')
               )
             )
+            .append(
+              $('<button type="button" class="delete-icon">')
+              .append(
+                $('<img src="img/icons/delete-icon.svg" alt="delete-icon">')
+              )
+            )
+          )
 
-          ).appendTo($('#milk'));
-          console.log($('#milk'));
-      // }
-    })
+        ).prependTo($('#milk'));
+      console.log(item_elem);
+    });
   }
+  $('.add-new').on('click', function(){
+    $('<div class="item">')
+    .append(
+      $('<div class="content-wrapper">')
+      .append(
+        $('<div class="left-content">')
+        .append(
+          $('<p class="circle">')
+        )
+        .append(
+          $('<p class="item-name">')
+        )
+      )
+      .append(
+        $('<div class="right-content">')
+        .append(
+          $('<p class="price">')
+          .append(
+            $('<span class="currency">')
+          )
+        )
+        .append(
+          $('<button type="button" class="edit-icon">')
+          .append(
+            $('<img src="img/icons/edit-icon.svg" alt="edit-icon">')
+          )
+        )
+        .append(
+          $('<button type="button" class="delete-icon">')
+          .append(
+            $('<img src="img/icons/delete-icon.svg" alt="delete-icon">')
+          )
+        )
+      )
+
+    ).insertBefore($(this));
+  });
 });
